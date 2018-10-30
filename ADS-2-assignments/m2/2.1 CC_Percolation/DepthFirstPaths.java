6b70e595b93fea1695e1c399ab13584228fe777f
rohith-22 <rohithkottauppari@msitprogram.net>
2018-10-30 20:44:09 +0530

    "ads2_m2_a1_1: 24/24 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2-assignments/m2/2.1 CC_Percolation/DepthFirstPaths.java
+++ ADS-2-assignments/m2/2.1 CC_Percolation/DepthFirstPaths.java
@@ -1,13 +1,29 @@
-// package edu.princeton.cs.algs4;
 
 /**
  *
  *  @author Robert Sedgewick
  *  @author Kevin Wayne
  */
+
+/**
+ * Class for depth first paths.
+ */
 class DepthFirstPaths {
-  private boolean[] marked;    // marked[v] = is there an s-v path?
-  private int[] edgeTo;        // edgeTo[v] = last edge on s-v path
+  /**
+   * { var_description }.
+   */
+  private boolean[] marked;
+  /**
+   * { item_description }.
+   */// marked[v] = is there an s-v path?
+  private int[] edgeTo;
+  /**
+   * { item_description }.
+   *edgeTo[v] = last edge on s-v path
+   */
+  /**
+   * { var_description }.
+   */
   private final int s;         // source vertex
 
   /**
@@ -25,6 +41,13 @@ class DepthFirstPaths {
   }
 
   // depth first search from v
+
+  /**
+   * { function_description }.
+   *
+   * @param      G     { parameter_description }
+   * @param      v     { parameter_description }
+   */
   private void dfs(Graph G, int v) {
     marked[v] = true;
     for (int w : G.adj(v)) {
@@ -36,7 +59,7 @@ class DepthFirstPaths {
   }
 
   /**
-   * Is there a path between the source vertex {@code s} and vertex {@code v}?
+   * Is there a path between the source vertex.
    * @param v the vertex
    * @return {@code true} if there is a path, {@code false} otherwise
    * @throws IllegalArgumentException unless {@code 0 <= v < V}
@@ -45,30 +68,16 @@ class DepthFirstPaths {
     validateVertex(v);
     return marked[v];
   }
-
   /**
-   * Returns a path between the source vertex {@code s} and vertex {@code v}, or
-   * {@code null} if no such path.
-   * @param  v the vertex
-   * @return the sequence of vertices on a path between the source vertex
-   *         {@code s} and vertex {@code v}, as an Iterable
-   * @throws IllegalArgumentException unless {@code 0 <= v < V}
+   * { function_description }.
+   *
+   * @param      v     { parameter_description }
    */
-  // public Iterable<Integer> pathTo(int v) {
-  //     validateVertex(v);
-  //     if (!hasPathTo(v)) return null;
-  //     Stack<Integer> path = new Stack<Integer>();
-  //     for (int x = v; x != s; x = edgeTo[x])
-  //         path.push(x);
-  //     path.push(s);
-  //     return path;
-  // }
-
-  // throw an IllegalArgumentException unless {@code 0 <= v < V}
   private void validateVertex(int v) {
     int V = marked.length;
     if (v < 0 || v >= V)
       throw new IllegalArgumentException("vertex " + v
-                                         + " is not between 0 and " + (V - 1));
+                                         + " is not between 0 and " +
+                                         (V - 1));
   }
 }
