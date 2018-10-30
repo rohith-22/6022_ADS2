78cfa363c668625ccd0f76fb0b9112a5f8561e23
rohith-22 <rohithkottauppari@msitprogram.net>
2018-10-30 20:49:17 +0530

    "ads2_m2_a1_1: 24/24 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2-assignments/m2/2.1 CC_Percolation/Graph.java
+++ ADS-2-assignments/m2/2.1 CC_Percolation/Graph.java
@@ -1,134 +1,114 @@
-
-import java.util.NoSuchElementException;
-
 /**
- gorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
- *
- *  @author Robert Sedgewick
- *  @author Kevin Wayne
+ * Class for graph.
  */
 class Graph {
-  private static final String NEWLINE =
-    System.getProperty("line.separator");
+    /**
+     * integer variable vertices.
+     */
+    private  int vertices;
+    /**
+     * integer variable edges.
+     */
+    private int edges;
+    /**
+     * array of bag type.
+     */
+    private Bag<Integer>[] adj;
+    /**
+     * Constructs the object.
+     */
+    Graph() {
 
-  private final int V;
-  private int E;
-  private Bag<Integer>[] adj;
-
-  /**
-   * Initializes an empty graph with {@code V} v.
-   * param V the number of vertices
-   *
-   * @param  V number of vertices
-   * @throws IllegalArgumentException if {@code V < 0}
-   */
-  public Graph(int V) {
-    if (V < 0) throw new
-      IllegalArgumentException
-      ("Number of vertices must be nonnegative");
-    this.V = V;
-    this.E = 0;
-    adj = (Bag<Integer>[]) new Bag[V];
-    for (int v = 0; v < V; v++) {
-      adj[v] = new Bag<Integer>();
     }
-  }
-
-
-  /**
-   * Returns the number of vertices in this graph.
-   *
-   * @return the number of vertices in this graph
-   */
-  public int V() {
-    return V;
-  }
-
-  /**
-   * Returns the number of edges in this graph.
-   *
-   * @return the number of edges in this graph
-   */
-  public int E() {
-    return E;
-  }
-
-  // throw an IllegalArgumentException unless {@code 0 <= v < V}
-  private void validateVertex(int v) {
-    if (v < 0 || v >= V)
-      throw new IllegalArgumentException("vertex " +
-                                         v + " is not between 0 and " +
-                                         (V - 1));
-  }
-
-  /**
-   * Adds the undirected edge v-w to this graph.
-   *
-   * @param  v one vertex in the edge
-   * @param  w the other vertex in the edge
-   * @throws IllegalArgumentException unless both
-   */
-  public void addEdge(int v, int w) {
-    validateVertex(v);
-    validateVertex(w);
-    E++;
-    adj[v].add(w);
-    adj[w].add(v);
-  }
-
-
-  /**
-   * Returns the vertices adjacent to vertex {@code v}.
-   *
-   * @param  v the vertex
-   * @return the vertices adjacent to vertex {@code v}, as an it.
-   * @throws IllegalArgumentException unless {@code 0 <= v < V}
-   */
-  public Iterable<Integer> adj(int v) {
     /**
-     * { item_description }.
+     * Constructs the object.
+     * Time complexity : O(n).
+     * @param      vertix     integer variable.
      */
-    validateVertex(v);
-    /**
-     * { item_description }.
-     */
-    return adj[v];
-  }
-
-  /**
-   * Returns the degree of vertex {@code v}.
-   *
-   * @param  v the vertex
-   * @return the degree of vertex {@code v}
-   * @throws IllegalArgumentException unless
-   */
-  public int degree(int v) {
-    /**
-     * { item_description }.
-     */
-    validateVertex(v);
-    return adj[v].size();
-  }
-
-
-  /**
-   * Returns a string representation of this graph.
-   *
-   * @return the number of vert.
-   *         followed by the <em>V</em> adjacency lists
-   */
-  public String toString() {
-    StringBuilder s = new StringBuilder();
-    s.append(V + " vertices, " + E + " edges " + NEWLINE);
-    for (int v = 0; v < V; v++) {
-      s.append(v + ": ");
-      for (int w : adj[v]) {
-        s.append(w + " ");
-      }
-      s.append(NEWLINE);
+    Graph(final int vertix) {
+        this.vertices = vertix;
+        this.edges = 0;
+        adj = (Bag<Integer>[]) new Bag[vertix];
+        for (int i = 0; i < vertix; i++) {
+            adj[i] = new Bag<Integer>();
+        }
     }
-    return s.toString();
-  }
-
-
+    /**
+     * returns vertices.
+     * Time complexity O(1).
+     * @return  vertices.
+     */
+    public int ve() {
+        return vertices;
+    }
+    /**
+     * returns edges.
+     * Time complexity : O(1).
+     * @return edges.
+     */
+    public int e() {
+        return edges;
+    }
+    /**
+     * Adds an edge.
+     * Time complexity O(1)
+     * @param      v     integer variable.
+     * @param      w     integer variable.
+     */
+    public void addEdge(final int v, final int w) {
+        if (!hasEdge(v, w)) {
+            edges++;
+            adj[v].add(w);
+            adj[w].add(v);
+        }
+        if (v == w) {
+            return;
+        }
+    }
+    /**
+     * Determines if it has edge.
+     *
+     * @param      v     integer variable.
+     * @param      w     integer variable.
+     * Time complexity O(v)
+     * @return     True if has edge, False otherwise.
+     */
+    public boolean hasEdge(final int v, final int w) {
+        if (adj[v] == null) {
+            return true;
+        }
+        for (int i : adj[v]) {
+            if (i == w) {
+                return true;
+            }
+        }
+        return false;
+    }
+    /**
+     * iterable function.
+     *
+     * @param      v integer variable.
+     * Time complexity : O(v)
+     * @return   array.
+     */
+    public Iterable<Integer> adj(final int v) {
+        return adj[v];
+    }
+    /**
+     * matrix method.
+     * Time complexity : O(1)
+     * @return   array.
+     */
+    public Bag[] matrix() {
+        return adj;
+    }
+    /**
+     * list method.
+     * Time complexity : O(1)
+     * @return  array.
+     */
+    public Bag[] list() {
+        return adj;
+    }
 }
+
