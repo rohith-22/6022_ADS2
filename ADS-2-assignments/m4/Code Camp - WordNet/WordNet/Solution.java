801c3681c73e32a81f36ce51f110b99090209d0d
rohith-22 <rohithkottauppari@msitprogram.net>
2018-11-05 00:43:24 +0530

    "ads2_m4_a1_1: 15/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2-assignments/m4/Code Camp - WordNet/WordNet/Solution.java
+++ ADS-2-assignments/m4/Code Camp - WordNet/WordNet/Solution.java
@@ -19,30 +19,30 @@ class Solution {
         String synset = sc.nextLine();
         String hypernyms = sc.nextLine();
         try {
-            WordNet w = new WordNet(synset, hypernyms);
+            WordNet wN = new WordNet(synset, hypernyms);
             String inputType = sc.nextLine();
-            if (w.gethasCycle()) {
+            if (wN.gethasCycle()) {
                 System.out.println("Cycle detected");
                 return;
             }
 
             if (inputType.equals("Graph")) {
-                w.checkMultipleRoots();
-                if (w.gethasMultipleRoots()) {
+                wN.checkMultipleRoots();
+                if (wN.gethasMultipleRoots()) {
                     return;
                 } else {
 
-                    System.out.println(w.getDigraph());
+                    System.out.println(wN.getDigraph());
                 }
             }
             if (inputType.equals("Queries")) {
                 while (sc.hasNextLine()) {
                     String[] tokens = sc.nextLine().split(" ");
                     try {
-                        w.sap(tokens[0], tokens[1]);
+                        wN.sap(tokens[0], tokens[1]);
                         System.out.println("distance = "
-                         + w.distance(tokens[0], tokens[1])
-                          + ", ancestor = " + w.sap(tokens[0], tokens[1]));
+                         + wN.distance(tokens[0], tokens[1])
+                          + ", ancestor = " + wN.sap(tokens[0], tokens[1]));
 
                     } catch (Exception e) {
                         System.out.println("IllegalArgumentException");
